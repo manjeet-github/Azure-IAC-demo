f951165ae73aa34388508bc1a50e1fb9af7af07c
Manjeet <manjeet@hashicorp.com>
2020-11-19 13:50:32 -0500

    modified main.tf - vm-size

--- main.tf
+++ main.tf
@@ -86,6 +86,7 @@ resource "azurerm_virtual_machine" "vm" {
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
   vm_size               = "Standard_D2_v2"
+# vm_size               = "Standard_B2s"
 
   storage_os_disk {
     name              = "myOsDisk"
